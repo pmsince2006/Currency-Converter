a66f58e166581a83e49112da3862791bc2e16463
Nishant <nishant0593.becse24@chitkara.edu.in>
2025-05-01 12:05:24 +0530

    test

---
+++ about.js
@@ -0,0 +1,9 @@
+// Simple script to dynamically set the about text
+document.addEventListener("DOMContentLoaded", () => {
+    const intro = document.getElementById("intro-text");
+  
+    intro.textContent = `Currency Converter is your reliable tool for real-time currency exchange rates. 
+    Our mission is to make global transactions simple, transparent, and accessible for everyone. 
+    Whether you're a traveler, business owner, or trader, we've got you covered.`;
+  });
+  
